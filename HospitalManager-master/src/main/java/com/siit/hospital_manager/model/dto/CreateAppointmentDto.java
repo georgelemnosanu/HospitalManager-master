687dfe5cd530da033dfe2918b1f51326d07e1a5c
georgelemnosanu <georgelemnosanu@gmail.com>
2023-01-07 04:26:16 +0200

    last

--- HospitalManager-master/src/main/java/com/siit/hospital_manager/model/dto/CreateAppointmentDto.java
+++ HospitalManager-master/src/main/java/com/siit/hospital_manager/model/dto/CreateAppointmentDto.java
@@ -1,10 +1,13 @@
 package com.siit.hospital_manager.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.siit.hospital_manager.model.Doctor;
 import com.siit.hospital_manager.model.Patient;
+import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 @Getter
@@ -12,7 +15,12 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class CreateAppointmentDto {
 
+    @Column(name = "date")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime date;
+
+
     private Patient patient;
     private Doctor doctor;
 
